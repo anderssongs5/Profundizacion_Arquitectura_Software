633937cd2cb2e5449fe0dd3de859d88004d2c8d1
Miguel Ángel Ossa Ruiz <miguelcold8@gmail.com>
2015-01-31 16:24:34 -0500

    Adding comments to Theatre Web Service class.

--- RTFs/RTF_2/Cinema_ProfArq/src/java/co/edu/udea/profarq/cinema/webservice/soap/impl/TheaterWebServiceImpl.java
+++ RTFs/RTF_2/Cinema_ProfArq/src/java/co/edu/udea/profarq/cinema/webservice/soap/impl/TheaterWebServiceImpl.java
@@ -20,6 +20,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.context.support.SpringBeanAutowiringSupport;
 
 /**
+ * Clase que implementa una interfaz, la cual le provee un Servicio Web para ser 
+ * ejecutado a trav&eacute;s de la tecnolog&iacute;a <b>S.O.A.P</b>, permitiendo
+ * en este caso el paso de mensajes entre dos o más sistemas.
+ * <p>
+ * La funci&oacute;n de esta clase es implementar los m&eacute;todos expuestos 
+ * por la interfaz de manera que ofrezcan el acceso a las funciones orientadas 
+ * al manejo de la entidad {@code Theater}.
+ * <p>
+ * Al ser un Servicio Web a trav&eacute;s de S.O.A.P., todos los m&eacute;todos
+ * o funciones aqu&iacute; implementadas est&aacute;n orientadas al acceso o
+ * invocaci&oacute;n de m&eacute;todos o funciones relacionados con la
+ * l&oacute;gica de la aplicaci&oacute;n y los procesos del negocio del Cinema 
+ * orientados al manejo de la entidad {@code Theater}.
  *
  * @author Andersson Garc&iacute;a Sotelo
  * @author Miguel &Aacute;ngel Ossa Ruiz
@@ -30,32 +43,76 @@ import org.springframework.web.context.support.SpringBeanAutowiringSupport;
 @XmlSeeAlso(value = {City.class, Theater.class, TheaterPK.class})
 public final class TheaterWebServiceImpl implements ITheaterWebService {
 
+    /**
+     * Variable global final est&aacute;tica que representa un tag que 
+     * identifica la clase, de manera que pueda ser identificada en diversos
+     * procesos que se lleven a cabo dentro de la aplicaci&oacute;n.
+     */
     private static final String TAG = TheaterWebServiceImpl.class.getName();
 
+     /**
+     * Entidad inyectada que es utilizada para el manejo a nivel de la capa de 
+     * persistencia en la aplicaci&oacute;n orientada a cada una de las 
+     * operaciones que puedan realizarse sobre la entidad {@code Theatre}.
+     */
     @Autowired()
     private ITheaterDAO theaterDAO;
 
+    /**
+     * Constructor p&uacute;blico que permite generaci&oacute;n de instancias 
+     * de esta clase. De es modo, crea un nuevo servicio web orientado a las 
+     * operaciones que pueden realizarse sobre la entidad {@code Theatre} y que
+     * son expuestas para que sean consumidas por un cliente.
+     */
     public TheaterWebServiceImpl() {
         super();
     }
-
+    
+    /**
+     * 
+     */
     @PostConstruct()
     @WebMethod(exclude = true)
     public void init() {
         SpringBeanAutowiringSupport.processInjectionBasedOnCurrentContext(this);
     }
 
+    
+    /**
+     * 
+     */
     @WebMethod(exclude = true)
     public ITheaterDAO getTheaterDAO() {
 
         return (this.theaterDAO);
     }
 
+    
+    /**
+     * 
+     */
     @WebMethod(exclude = true)
     public void setTheaterDAO(ITheaterDAO theaterDAO) {
         this.theaterDAO = theaterDAO;
     }
 
+       /**
+     * M&eacute;todo o funci&oacute;n del Servicio Web que implementa un punto 
+     * de invocaci&oacute;n al m&eacute;todo o proceso relacionados con las
+     * instancias o entidades {@code Theater}, las cuales representan los
+     * Teatros ubicados en cierta Ciudad de inter&eacute;s.
+     * <p>
+     * El m&eacute;todo retorna una lista de objetos que definen los Teatros
+     * activos en cierta Ciudad, y los retorna como recurso en caso de encontrar
+     * valores en el repositorio de datos asociado a la aplicacio&acute;n. En el
+     * caso que no hayan registros asociados y encontrados, el m&eacute;todo o
+     * funci&oacute;n devolver&aacute; una lista vac&iacute;a sin datos.
+     *
+     * @param cityCode Objeto que representa el c&oacute;digo de la Ciudad en la
+     * cual se desea efectuar la b&uacute;squeda de sus Teatros o localidadas.
+     * @return Lista que contiene toda la informaci&oacute;n de los Teatros
+     * encontrados exitosamente en la Ciudad de inter&eacute;s.
+     */
     @Override()
     @WebMethod(
             operationName = SOAPWebServiceContract.TheaterWebService.FIND_THEATERS_BY_CITY_OPERATION)
